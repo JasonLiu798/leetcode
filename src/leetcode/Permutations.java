c49acdc80b5aae793c217cecabcfd24fd9e740e0
Jason <jasondliu@qq.com>
2014-09-14 11:14:25 +0800

    Permutations

--- src/leetcode/Permutations.java
+++ src/leetcode/Permutations.java
@@ -16,20 +16,75 @@ import java.util.List;
  */
 public class Permutations {
 	
-//	/**
-//	 * fix 0~n-2,swap n-1,n,output 1
-//	 * fix 0~
-//	 * 
-//	 * @param num
-//	 * @return
-//	 */
-//	public List<List<Integer>> permute_fix(int[] num) {
-//		return null;
-//	}
+	/**
+	 * Tool function
+	 * @param l
+	 */
+	public static void printList(List l){
+		for(int i=0;i<l.size();i++){
+			System.out.print(l.get(i)+",");
+		}
+		System.out.println("");
+	}
+	
+	/**
+	 * v3 swap method
+	 * 
+	 * new lists(oldlists);
+	 * swap(lists[idx+1],lists[0]~lists[idx-1])
+	 * 
+	 * Accepted	476 ms	java
+	 */
+	public static List<List<Integer>> permute(int[] num) {
+		List<List<Integer>> res =  new ArrayList();
+		if(num.length<=0){
+			return res;
+		}else if(num.length==1){
+			List tmp = new ArrayList();
+			tmp.add(num[0]);
+			res.add(tmp);
+			return res;
+		}else{
+			//add first
+			List<Integer> first = new LinkedList();
+			for(int i=0;i<num.length;i++){
+				first.add(i,num[i]);
+			}
+			res.add(first);
+			List<Integer> tmpListP = null;
+			List<Integer> tmpNewListP = null;
+			//Iterator<List<Integer>> itr = null;//res.iterator();
+			//swap list
+			for(int idx=1;idx<num.length;idx++){
+				int resLen = res.size();
+				for(int resItr=0;resItr<resLen;resItr++){
+					tmpListP = res.get(resItr);
+					for(int tmpListItr=0;tmpListItr<idx;tmpListItr++){//swap 0~idx,last
+						tmpNewListP = new LinkedList(tmpListP);
+						int tmp = tmpNewListP.get(tmpListItr);
+						tmpNewListP.set(tmpListItr,tmpNewListP.get(idx));
+						tmpNewListP.set(idx,tmp);
+						res.add(res.size(),tmpNewListP);
+					}
+//					System.out.println("====AF SWAP====");
+//					for(int i=0;i<res.size();i++){
+//						printList(res.get(i));
+//					}
+					
+				}
+				
+			}
+		}
+		return res;
+	}
+	
+
 	
 	
 	
 	/**
+	 * Deprecated,too many duplications,must clean duplication
+	 * v2
 	 * 1.get one
 	 * 		no got: goto end
 	 * 		got: goto2
@@ -40,7 +95,7 @@ public class Permutations {
 	 * return lists
 	 * @param num
 	 * @return
-	 */
+	 *
 	public static List<List<Integer>> permute(int[] num) {
 		List<List<Integer>> res =  new ArrayList();
 		
@@ -60,7 +115,7 @@ public class Permutations {
 			 * 		no: goto1
 			 * 		got: 
 			 * 			copy old list,insert into one gap,goto 2
-			 */
+			 *
 			//Iterator<List<Integer>> itr = res.iterator();
 			List<Integer> tmpListp = null;
 			List<Integer> tmpListBFInsertp = null;//new LinkedList(tmpListp);
@@ -121,34 +176,17 @@ printList(tmpListp);
 		return res;
 	}
 	
-	public static void printList(List l){
-		for(int i=0;i<l.size();i++){
-			System.out.print(l.get(i)+",");
-		}
-		System.out.println("");
-	}
-//			List tmp = new ArrayList();
-//			tmp.add(num[0]);
-//			tmp.add(num[1]);
-//			res.add(tmp);
-//			tmp = new ArrayList();
-//			tmp.add(num[1]);
-//			tmp.add(num[0]);
-//		}else{
-//			boolean[] isFix=new boolean[num.length];
-//			for( boolean itr:isFix){
-//				itr=true;
-//			}
-//			List<List<Integer>> res = new ArrayList();
-//			
-//			for(int i=0;i<num.length;i++){
-//				
-//				for(int j=num.length-1;j>num.length-2;j++){
-//					
-//				}
-//			}
-//		}
+	
+	List<List<Integer>> res = new LinkedList();
+	
+	
+	
+	public static void permute_(List<Integer> L){
 		
+	}
+	
+	
+	
 	public static List<List<Integer>> findSame(List<List<Integer>> ls){
 		List<List<Integer>> res = new LinkedList();
 		List<Integer> L1 = null;
@@ -173,54 +211,121 @@ printList(tmpListp);
 		
 		return res;
 	}
-
+	*/
 	
-//	public static List<List<Integer>> deleteSame(List<List<Integer>> ls,List){
-//		
+//	/**
+//	 * v1
+//	 *	fix 0~n-2,swap n-1,n,output 1
+//	 * fix 0~
+//	 * 
+//	 * @param num
+//	 * @return
+//	 */
+//	public List<List<Integer>> permute_fix(int[] num) {
+//		return null;
 //	}
 	
+	/*
+	 * stick some
+	1s 2 3
+	1s 3 2
+	
+	3 2s 1
+		3s 1 2
+	2 1 3s
+		2s 3 1
+		
+	1s 
+		2s 3 4
+		2s 4 3
+		
+		4 3s 2
+			4s 2 3
+		4 2 3s
+			4s 3 2
+-------
+	1 2 3
+	1 3 2
+	
+	
+			
+	*/	
+	/*
+	List<Integer> save;//=  new LinkedList();
+	List<Boolean> visit = new  LinkedList();
+	List<List<Integer>> v = new LinkedList();
+	public List<List<Integer>> permute(int[] num) {
+		v.clear();
+	    int size = num.length;
+	    save =  new LinkedList();
+	    for(int i=0;i<size;i++){
+	    	visit.add(false);
+	    	save.add(0);
+	    }
+	    
+//	    visit.resize(size);
+//	    save.resize(size);
+
+	    //fill(visit.begin(), visit.end(), false);
+	    
+	    dfs(num, 0, size,save);
+	    return v;
+	}
+	
+	void dfs(int[] num, int now, int size,List<Integer> sv){
+		save =  new LinkedList(sv);
+//		for(int i=0;i<size;i++){
+//	    	visit.add(false);
+//	    	save.add(0);
+//	    }
+	    if(now == size){
+	    	
+	        v.add(save);
+	        return;
+	    }
+	    
+	    for(int i = 0; i < size; i++){
+	    	boolean vb;
+	    	printList(visit);
+	        if(!(vb = visit.get(i)) ){
+	            visit.set(i, true);
+	            save.set(now, num[i]);
+	            
+	            dfs(num, now + 1, size,save);
+	            visit.set(i, false);
+	        }
+	    }
+	}
+	*/
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int[] a={1,2,3,4};
-		List<List<Integer>> res = permute(a);
+		List<List<Integer>> res = new Permutations().permute(a);
 		System.out.println("++++++RES:");
 		for(int m=0;m<res.size();m++){
 			printList(res.get(m));
 		}
-		List<List<Integer>> sameres = findSame(res);
-		System.out.println("++++++SAME RES:");
-		for(int m=0;m<sameres.size();m++){
-			printList(sameres.get(m));
-		}
-		
-		Iterator<List<Integer>> itr = sameres.iterator();
-		while(itr.hasNext()){
-			res.remove(itr.next());
-		}
-		
-		System.out.println("++++++AF RES:");
-		for(int m=0;m<res.size();m++){
-			printList(res.get(m));
-		}
 		
 		
 		
-		/*
-		//12 3//end
-		1 3 2
-	//3 12
-		//13 2//end 
-		1 2 3
-	//2 13
-		//21 3//end
-		2 3 1
-		3 21
-		//23 1//end
-		2 1 3
-	//1 23
-	 * 
-	 */
+//		List<List<Integer>> sameres = findSame(res);
+//		System.out.println("++++++SAME RES:");
+//		for(int m=0;m<sameres.size();m++){
+//			printList(sameres.get(m));
+//		}
+		
+//		Iterator<List<Integer>> itr = sameres.iterator();
+//		while(itr.hasNext()){
+//			res.remove(itr.next());
+//		}
+//		
+//		System.out.println("++++++AF RES:");
+//		for(int m=0;m<res.size();m++){
+//			printList(res.get(m));
+//		}
+//		
+		
+		
 	}
 
 }
