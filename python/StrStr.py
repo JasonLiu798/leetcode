65ab79a98dccae68b33db13b36803813e3ffe7f6
jianlong <jianlong@juanpi.com>
2016-01-19 11:25:12 +0800

    bm

--- python/StrStr.py
+++ python/StrStr.py
@@ -48,6 +48,25 @@ class Solution(object):
                 return i - nlen + 1
         return -1
 
+    #bm
+    def BoyerMooreHorspool(self,pattern, text):
+        m = len(pattern)
+        n = len(text)
+        if m > n: return -1
+        skip = []
+        for k in range(256): skip.append(m)
+        for k in range(m - 1): skip[ord(pattern[k])] = m - k - 1
+        skip = tuple(skip)
+        k = m - 1
+        while k < n:
+            j = m - 1; i = k
+            while j >= 0 and text[i] == pattern[j]:
+                j -= 1; i -= 1
+            if j == -1: return i + 1
+            k += skip[ord(text[k])]
+        return -1
+
+
     #TLE
     def strStr1(self, haystack, needle):
         """
