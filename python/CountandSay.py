9051263c19ec32e51d38faffde2a909686e12813
JasonLiu798 <jasondliu@qq.com>
2016-01-26 11:20:36 +0800

    CountandSay

--- python/CountandSay.py
+++ python/CountandSay.py
@@ -20,32 +20,53 @@ class Solution(object):
         :type n: int
         :rtype: str
         """
-        res=''
-        cur=0
+        curstr='1'
+        if n<=0:
+            return ''
+        if n==1:
+            return '1'
+        i=0
+        while i<n-1:
+            curstr=self.genOne(curstr)
+            i+=1
+        return curstr
 
     def genOne(self,s):
+        res=''
         if s=='':
             return '1'
         idx=0
         while idx<len(s):
             nxt=idx+1
+            # print 'idx',idx,'nxt',nxt
             if nxt<len(s):
-                dupCnt=0
-                while s[nxt]==s[idx]:
+                dupCnt=1
+                while nxt<len(s) and s[nxt]==s[idx]:
                     nxt+=1
                     dupCnt+=1
                 if dupCnt>1:
-                    res+=str(dupCnt)+'1'
+                    res+=str(dupCnt)+s[idx]
                     idx+=dupCnt
                 else:
                     res+='1'+str(s[idx])
                     idx+=1
             else:
-                if s[idx:nxt]=='1':
-                    res+='11'
-
+                res+='1'+s[idx:nxt]
+                idx+=1
         return res
 
 if __name__ == '__main__':
     s = Solution()
-    res = s.countAndSay(3)
+    res = s.genOne('111221')
+    # res = s.countAndSay(4)
+    #1 1
+    #2 11
+    #3 21
+    #4 1211
+    #5 111221
+    #6 312211
+    print res
+
+
+
+
